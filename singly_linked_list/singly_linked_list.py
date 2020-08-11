5ef7bc222e1bb0cdd0861c9263bc4212686fc9fe
Anthony Carrillo <carrilloanthony2698@gmail.com>
2020-08-10 20:33:17 -0400

    Added Node class.

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, value = None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.get_value
+    
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+class LinkedList:
+    def __init__(self)
+        self.head = None
+        self.tail = None
+    
+    
