fe33447ec046691005992d0bbeae8ed572fdabc5
Anthony Carrillo <carrilloanthony2698@gmail.com>
2020-08-11 12:25:59 -0400

    Working on stack file.

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -4,7 +4,7 @@ class Node:
         self.next_node = next_node
 
     def get_value(self):
-        return self.get_value
+        return self.value
     
     def get_next(self):
         return self.next_node
@@ -13,8 +13,54 @@ class Node:
         self.next_node = new_next
 
 class LinkedList:
-    def __init__(self)
+    def __init__(self):
         self.head = None
         self.tail = None
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
     
+    def remove_tail(self):
+        if self.head is None and self.tail is None:
+            return None
+        
+        if self.head == self.tail:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return value
+        else:
+            value = self.tail.get_value()
+            current = self.head
+            while current.get_next() != self.tail:
+                current = current.get_next
+            self.tail = current
+            self.tail.set_next(None)
+            return value
     
+    def remove_head(self):
+        if self.head is None and self.tail is None:
+            return None
+        
+        if self.head == self.tail:
+            val = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return val
+        else:
+            val = self.head.get_value()
+            self.head = self.head.get_next()
+            return val
+
+
+
+
+
+
