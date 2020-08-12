b2946142da597ae4137194ec0c8587d4baf80e2c
anthony2698 <carrilloanthony2698@gmail.com>
2020-08-12 08:09:25 -0400

    Added stacks file.

--- stack/stack.py
+++ stack/stack.py
@@ -9,8 +9,9 @@ return elements in Last In First Out order.
    Make sure the Stack tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
+
+   When using an array the runtime complexity is 0(n), while a linked list provides the fastest runtime complexity 0(1) when pushing or popping nodes.
 """
-from singly_linked_list import LinkedList
 
 class Stack:
     def __init__(self):
@@ -18,10 +19,76 @@ class Stack:
         self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_tail(value)
 
     def pop(self):
-        pass
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.storage.remove_tail()
+
+#Linked List, Node Classes code 
+class Node:
+    def __init__(self, value = None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+    
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
+    
+    def remove_tail(self):
+        if self.head is None and self.tail is None:
+            return None
+        
+        if self.head == self.tail:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return value
+        else:
+            value = self.tail.get_value()
+            current = self.head
+            while current.get_next() != self.tail:
+                current = current.get_next()
+            self.tail = current
+            self.tail.set_next(None)
+            return value
+    
+    def remove_head(self):
+        if self.head is None and self.tail is None:
+            return None
+        
+        if self.head == self.tail:
+            val = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return val
+        else:
+            val = self.head.get_value()
+            self.head = self.head.get_next()
+            return val
